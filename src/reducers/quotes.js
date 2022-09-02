3e727964ccc0e01eb825508e69af010f8fc654dc
Chris Baum <Christopher.Baum@valpo.edu>
2022-09-02 10:55:13 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,38 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return state.concat(action.quote)
+
+    case "REMOVE_QUOTE":
+      return state.filter(state => state.id !== action.quoteId)
+
+    case "UPVOTE_QUOTE":
+      return (state.map(stateIn => {
+        if (stateIn.id === action.quoteId) {
+          return (
+            { ...stateIn, votes: stateIn.votes + 1 }
+          )
+        }
+        else {
+          return stateIn
+        }
+      }
+      ))
+
+    case "DOWNVOTE_QUOTE":
+      return (state.map(stateIn => {
+        if ((stateIn.id === action.quoteId) && (stateIn.votes > 0)) {
+          return (
+            { ...stateIn, votes: stateIn.votes - 1 }
+          )
+        }
+        else {
+          return stateIn
+        }
+      }
+      ))
+
+    default:
+      return state
+  }
 }
